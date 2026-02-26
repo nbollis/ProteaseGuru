e3cbaad6f46767719f4514861db938843e38331c
nbollis <nbollis@comcast.net>
2026-02-26 13:10:45 -0600

    globals

---
+++ GuiFunctions/GuiGlobalParams.cs
@@ -0,0 +1,7 @@
+namespace GuiFunctions;
+
+// POCO class representing the global parameters for the GUI functions. This class can be used to store and manage global parameters that are needed across different parts of the application.
+public class GuiGlobalParams
+{
+    public string? GlobalParams { get; set; }
+}
